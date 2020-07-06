493b4102d785536937b23ad1a998ba432d953e8c
YOSHI091993 <krs0969@gmail.com>
2020-07-06 18:02:19 +0900

    仮

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 吉和　匠 on 2020/06/11.
+//  Copyright © 2020 SHO Yoshiwa. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
---
+++ Instagram/PostData.swift
@@ -0,0 +1,53 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by 吉和　匠 on 2020/06/12.
+//  Copyright © 2020 SHO Yoshiwa. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PostData: NSObject {
+    var id: String
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    var comments: [String] = []
+    var isCommented: Bool = false
+    
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        
+        let postDic = document.data()
+        
+        self.name = postDic["name"] as? String
+        
+        self.caption = postDic["caption"] as? String
+        
+        let timestamp = postDic["date"] as? Timestamp
+        self.date = timestamp?.dateValue()
+        
+        if let likes = postDic["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        if let myid  = Auth.auth().currentUser?.uid {
+            if self.likes.firstIndex(of: myid) != nil {
+                self.isLiked = true
+            }
+        }
+        
+        if let comments = postDic["comments"] as? [String] {
+            self.comments = comments
+        }
+        if let myid = Auth.auth().currentUser?.uid {
+            if self.comments.firstIndex(of: myid) != nil {
+                self.isCommented = true
+            }
+        }
+    }
+}
